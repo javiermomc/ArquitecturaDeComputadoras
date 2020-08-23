bf59d9cc2109ec7ee98fab4e06f95710bf1c44d7
Javier Mondragon <javiermomc@outlook.com>
2020-08-23 16:11:11 -0500

    Adder project from Modulos Basicos removed.

--- Lab/Projects/P01_ModulosBasicos/adder/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "D:/JavierJr/Documents/ISD/AgoDic20/ArquitecturaDeComputadoras/Lab/Projects/P01_ModulosBasicos/adder/adder_isim_beh.exe" -prj "D:/JavierJr/Documents/ISD/AgoDic20/ArquitecturaDeComputadoras/Lab/Projects/P01_ModulosBasicos/adder/adder_beh.prj" "work.adder" 
