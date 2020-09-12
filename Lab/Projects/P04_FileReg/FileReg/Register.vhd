7957f52cb7482e7d6d250d5ec6ad98ef187dcd62
mmorales01 <mmoralesdelavega21@gmail.com>
2020-09-11 22:06:28 -0500

    Me marca errores con el port32of32

--- Lab/Projects/P04_FileReg/FileReg/Register.vhd
+++ Lab/Projects/P04_FileReg/FileReg/Register.vhd
@@ -19,6 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.array_port.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,13 +31,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Register is
-	Port ( 
+	Port ( WriteData : in STD_LOGIC_VECTOR (31 DOWNTO 0);
+			 Enabler : in STD_LOGIC_VECTOR (31 DOWNTO 0);
+			 CLK : in STD_LOGIC);
+			 
 end Register;
 
 architecture Behavioral of Register is
-
-begin
-
-
+	signal DATA : PORT32OF32;
+	begin
+		for i in '0' to '31' generate
+      begin
+         DATA(i) <= WriteData when Enabler(i) = '1' AND i > '0' AND rising_edge(CLK);
+   end generate;
 end Behavioral;
 
