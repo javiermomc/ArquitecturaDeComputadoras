dbc41fd66fb7d7cdddf9844401a1d1236d7eeb26
Javier Mondragon <javiermomc@outlook.com>
2020-10-26 22:40:59 -0600

    Added format.

--- Lab/Projects/P07_UC/CU/ALUControl.vhd
+++ Lab/Projects/P07_UC/CU/ALUControl.vhd
@@ -39,8 +39,9 @@ end ALUControl;
 architecture Behavioral of ALUControl is
 signal Special : STD_LOGIC_VECTOR (2 downto 0);
 begin
+
 	with Instruc select
-		Special <= "000" when "100100", --AND
+		Special <=    "000" when "100100", --AND
 						  "001" when "100101", --OR
 						  "010" when "100000", --ADD
 						  "110" when "100010", --SUB
@@ -48,14 +49,15 @@ begin
 						  "101" when others;
 
 	with ALUOp select
-		ALUCtrl <= "010" when "100", --ADD / LW,SW,ADDI
+		ALUCtrl <=  "010" when "100", --ADD / LW,SW,ADDI
 						"110" when "101", --SUB / BEQ
 						"001" when "110", --OR /ORI
 						"100" when "111", --B UPPER / LUI
 						"101" when "010", --NOP
 						Special when "000", --TIPO R
-						"101" when others; --NOTHING
+						"000" when others; --NOTHING
 						
 	Jr <= '1' when ALUOp = "001" and Instruc = "001000" else '0';
+	
 end Behavioral;
 
